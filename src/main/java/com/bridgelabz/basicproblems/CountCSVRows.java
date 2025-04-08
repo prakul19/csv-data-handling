afdf30b210288128c3b1008f57a9f5262f36ec57
prakul19 <prakulagarwal5@gmail.com>
2025-04-08 10:32:49 +0530

    [prakul] stats: program for counting the rown in an csv file

---
+++ src/main/java/com/bridgelabz/basicproblems/CountCSVRows.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.basicproblems;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+public class CountCSVRows {
+    public static void main(String[] args) {
+        String filePath = "/Users/prakulagarwal/Desktop/bridgelabz-workspace/csv-data-handling/src/main/java/com/bridgelabz/basicproblems/employees.csv";
+        int rowCount = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            boolean isHeader = true;
+            while ((line = br.readLine()) != null) {
+                if (isHeader) {
+                    isHeader = false;
+                    continue;
+                }
+                rowCount++;
+            }
+            System.out.println("Total records (excluding header): " + rowCount);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
